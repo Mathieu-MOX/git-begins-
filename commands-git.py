02c4c705f4798b84d87aeca5ad638ec6d1295a8f
Mathieu-MOX <mathieu.mokhtar@laplateforme.io>
2025-10-24 10:21:13 +0200

    new projet

---
+++ commands-git.py
@@ -0,0 +1,10 @@
+#Dans le terminal et dans le repository cloné, créer un
+#  fichier nommé «commands-git », 
+# dans ce fichier, écrire les commandes qui permettent de :
+
+
+#➔ Cloner un repository
+#Les 3 commandes de push
+
+# pour cloner = git cloner https://github.com/Matieu-MO12/git-begins-.git
+# ensuite git add pour ajouter un fichier 
